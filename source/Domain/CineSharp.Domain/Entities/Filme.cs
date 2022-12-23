911f18086bb713a1abb0fa4f6ad0f96defb55d4b
Victor Ochoa <victor.silva@luizalabs.com>
2022-12-23 15:29:25 -0300

    fix: corrigido namespace

---
+++ source/Domain/CineSharp.Domain/Entities/Filme.cs
@@ -0,0 +1,17 @@
+﻿using CineSharp.Domain.Core.Entities;
+
+namespace CineSharp.Domain.Entities;
+
+public class Filme : Entity
+{
+    public Filme(string titulo, string sinopse, int duracao)
+    {
+        Titulo = titulo;
+        Sinopse = sinopse;
+        Duracao = duracao;
+    }
+
+    public string Titulo { get; private set; }
+    public string Sinopse { get; private set; }
+    public int Duracao { get; private set; }
+}
---
+++ source/Domain/CineSharp.Domain/Entities/Sessao.cs
@@ -0,0 +1,19 @@
+﻿using CineSharp.Domain.Core.Entities;
+
+namespace CineSharp.Domain.Entities;
+
+public class Sessao : Entity
+{
+    public Sessao(DateTime data, int lugares, decimal valorIngreco, Filme filme)
+    {
+        Data = data;
+        Lugares = lugares;
+        ValorIngreco = valorIngreco;
+        Filme = filme;
+    }
+
+    public DateTime Data { get; private set; }
+    public int Lugares { get; private set; }
+    public decimal ValorIngreco { get; private set; }
+    public Filme Filme { get; private set; }
+}

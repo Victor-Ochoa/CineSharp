61059ce8ed30dd5548c0ff24885702d235fa28a3
Victor Ochoa <victor.silva@luizalabs.com>
2022-12-23 16:31:23 -0300

    feat: add query

---
+++ source/Domain/CineSharp.Domain/Queries/GetSessaoQuery.cs
@@ -0,0 +1,18 @@
+﻿using CineSharp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CineSharp.Domain.Queries;
+
+public static class GetSessaoQuery
+{
+    public static Expression<Func<Sessao, bool>> GetSessaoByFilme(Guid FilmeId)
+    => x => x.Filme.Id== FilmeId;
+
+    public static Expression<Func<Sessao, bool>> GetSessaoByData(DateOnly data)
+        => x => DateOnly.FromDateTime(x.Data.Date) >= data;
+}
